d71abf6fe2d040b4f6a41717aba836147c9d8261
Ishani Ray <ishani_ray@yahoo.co.in>
2022-05-07 23:57:24 +0530

    Time: 22 ms (85.65%), Space: 42.8 MB (95.92%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,14 +1,13 @@
 class MyCalendar {
     
-    private TreeMap<Integer, Integer> bookings;
+    private TreeMap<Integer, Integer> bookings; // key = start time; value = end time
 
     public MyCalendar() {
         this.bookings = new TreeMap<>();
     }
     
     public boolean book(int start, int end) {
-        Map.Entry<Integer, Integer> prevBooking = bookings.lowerEntry(end);
-        if (prevBooking != null && prevBooking.getValue() > start) {
+        if (bookings.lowerEntry(end) != null && bookings.lowerEntry(end).getValue() > start) { // previous booking's end time > this booking's start time
             return false;
         }
         bookings.put(start, end);
